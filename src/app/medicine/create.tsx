a106a48258b833bae45f8d29cd2efaecc6076262
Gustavo-Nasc <gustavo.ns@outlook.com.br>
2024-06-07 03:41:54 -0300

    feat: started POST request to 'medicine'

--- src/app/medicine/create.tsx
+++ src/app/medicine/create.tsx
@@ -6,42 +6,226 @@ import { Feather } from '@expo/vector-icons'
 import colors from 'tailwindcss/colors'
 import { Input } from '@/src/components/input'
 import Animated, { FadeInUp } from 'react-native-reanimated'
+import { useToast } from 'native-base'
+import { useRouter } from 'expo-router'
+import { usePOSTMedicine } from '@/src/hooks/medicine/usePOSTMedine'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { MedicineSchema, medicineSchema } from '@/src/schemas/medicineSchema'
+import { Controller, useForm } from 'react-hook-form'
+import { useEffect } from 'react'
+import { formatDate } from '@/src/utils/formatDate'
+import { Option, Select } from '@/src/components/select'
+import { useGETProviders } from '@/src/hooks/provider/useGETProviders'
+import { Loading } from '@/src/components/loading'
 
 export default function CreateMedicine() {
+  const router = useRouter()
+  const toast = useToast()
+
+  const { mutate, data: requestError, isPending, isSuccess } = usePOSTMedicine()
+
+  const providersOptions: Option[] = []
+
+  const { data: providers, isLoading } = useGETProviders()
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<MedicineSchema>({
+    resolver: yupResolver(medicineSchema),
+  })
+
+  function handleCreateMedicine(medicine: MedicineSchema) {
+    console.log(medicine)
+  }
+
+  useEffect(() => {
+    if (!isSuccess) return
+
+    if (requestError) {
+      toast.show({
+        title: requestError,
+        placement: 'top',
+        textAlign: 'center',
+        bg: 'rose.400',
+      })
+    } else {
+      toast.show({
+        title: 'Fornecedor criado com sucesso',
+        placement: 'top',
+        textAlign: 'center',
+        bg: 'success.600',
+      })
+    }
+
+    return router.navigate('/provider/')
+  }, [isSuccess, requestError, toast, router])
+
+  useEffect(() => {
+    if (!isLoading) {
+      if (providers?.length === 0) {
+        toast.show({
+          title: 'Cadastre um Fornecedor antes',
+          placement: 'top',
+          textAlign: 'center',
+          bg: 'rose.400',
+        })
+  
+        return router.navigate('/medicine/')
+      }
+    }
+
+  }, [isLoading, providers])
+
+  providers?.forEach((provider) => {
+    const newOption = {
+      label: provider.name,
+      value: provider.id,
+    }
+
+    providersOptions.push(newOption)
+  })
+
   return (
     <View className="mx-5 mt-16 flex-1">
       <ReturnHeader title="Novo medicamento" />
 
-      <Animated.ScrollView entering={FadeInUp} contentContainerStyle={{ paddingVertical: 32 }}>
-        <View className="py-8">
+      {isLoading || !providers ? (
+        <Loading />
+      ) : (
+        <Animated.ScrollView
+          entering={FadeInUp}
+          contentContainerStyle={{ paddingVertical: 32 }}
+        >
           <View className="mb-12" style={{ gap: 16 }}>
-            <Input title="Nome" />
+            <Controller
+              control={control}
+              name="name"
+              render={({ field: { onChange } }) => (
+                <Input
+                  errorMessage={errors.name?.message}
+                  onChangeText={onChange}
+                  placeholder="NexGard Spectra"
+                  title="Nome"
+                />
+              )}
+            />
 
-            <Input title="Data de fabricação" />
+            <Controller
+              control={control}
+              name="manufacturingDate"
+              render={({ field: { onChange } }) => (
+                <Input
+                  errorMessage={errors.manufacturingDate?.message}
+                  onChangeText={onChange}
+                  title="Data de Fabricação"
+                  placeholder={formatDate(new Date())!}
+                />
+              )}
+            />
 
-            <Input title="Data de validade" />
+            <Controller
+              control={control}
+              name="expirationDate"
+              render={({ field: { onChange } }) => (
+                <Input
+                  errorMessage={errors.expirationDate?.message}
+                  onChangeText={onChange}
+                  title="Data de Validade"
+                  placeholder={formatDate(new Date())!}
+                />
+              )}
+            />
 
-            <Input title="utilidade" />
+            <Controller
+              control={control}
+              name="utility"
+              render={({ field: { onChange } }) => (
+                <Input
+                  errorMessage={errors.utility?.message}
+                  onChangeText={onChange}
+                  title="Utilidade"
+                  placeholder="Antipulgas, Carrapatos e Vermifugo"
+                />
+              )}
+            />
 
-            <Input title="Observação" multiline={true} />
+            <Controller
+              control={control}
+              name="observation"
+              render={({ field: { onChange } }) => (
+                <Input
+                  errorMessage={errors.observation?.message}
+                  onChangeText={onChange}
+                  title="Observação"
+                  placeholder="Para cães de 7,6 a 15 Kg"
+                  multiline
+                />
+              )}
+            />
 
-            <Input title="Quantidade" />
+            <Controller
+              control={control}
+              name="amount"
+              render={({ field: { onChange } }) => (
+                <Input
+                  errorMessage={errors.amount?.message}
+                  onChangeText={onChange}
+                  title="Quantidade"
+                  placeholder="100"
+                  keyboardType="number-pad"
+                />
+              )}
+            />
 
-            <Input title="Data de chegada" />
+            <Controller
+              control={control}
+              name="arrivalDate"
+              render={({ field: { onChange } }) => (
+                <Input
+                  errorMessage={errors.arrivalDate?.message}
+                  onChangeText={onChange}
+                  title="Data de Chegada"
+                  placeholder={formatDate(new Date())!}
+                />
+              )}
+            />
 
-            <Input title="Bula" />
+            <Controller
+              control={control}
+              name="leaflet"
+              render={({ field: { onChange } }) => (
+                <Input
+                  errorMessage={errors.leaflet?.message}
+                  onChangeText={onChange}
+                  title="Bula"
+                  multiline
+                  placeholder="A administração de NexGard será uma tarefa fácil, pois o tablete mastigável sabor carne é altamente aceito pelos cães, graças a sua alta palatabilidade"
+                />
+              )}
+            />
 
-            <Input title="Fornecedor" />
+            <Controller
+              control={control}
+              name="provider"
+              render={() => (
+                <Select options={providersOptions} title="Fornecedor" value={null} />
+              )}
+            />
           </View>
 
-          <Button.Root>
+          <Button.Root
+            disabled={isSubmitting || isPending}
+            onPress={handleSubmit(handleCreateMedicine)}
+          >
             <Button.Icon>
               <Feather name="plus-square" size={18} color={colors.slate[950]} />
             </Button.Icon>
-            <Button.Title>Cadastrar Medicamento</Button.Title>
+            <Button.Title>Cadastrar Fornecedor</Button.Title>
           </Button.Root>
-        </View>
-      </Animated.ScrollView>
+        </Animated.ScrollView>
+      )}
     </View>
   )
 }
--- src/schemas/medicineSchema.ts
+++ src/schemas/medicineSchema.ts
@@ -1,6 +1,50 @@
 import * as yup from 'yup'
 
-export const medicineSchema = yup.object({})
+export const medicineSchema = yup.object({
+  name: yup
+    .string()
+    .required('O nome é obrigatório')
+    .min(3, 'O nome precisa ter no mínimo 3 caracteres'),
+  manufacturingDate: yup
+    .string()
+    .required('A data de fabricação é obrigatório')
+    .min(10, 'A data de fabricação tem que ter 10 caracteres')
+    .max(10, 'A data de fabricação tem que ter 10 caracteres')
+    .matches(
+      /^\d{2}\/\d{2}\/\d{4}/,
+      'A data de fabricação precisa estar no formato correto (00/00/0000)',
+    ),
+  expirationDate: yup
+    .string()
+    .required('A data de validade é obrigatório')
+    .min(10, 'A data de validade tem que ter 10 caracteres')
+    .max(10, 'A data de validade tem que ter 10 caracteres')
+    .matches(
+      /^\d{2}\/\d{2}\/\d{4}/,
+      'A data de validade precisa estar no formato correto (00/00/0000)',
+    ),
+  utility: yup.string().required('A utilidade é obrigatória'),
+  observation: yup.string().optional().nullable(),
+  amount: yup
+    .number()
+    .required('A quantidade é obrigatória')
+    .min(1, 'A quantidade precisa ser superior a 1')
+    .integer('O número precisa ser inteiro'),
+  arrivalDate: yup
+    .string()
+    .required('A data de chegada é obrigatório')
+    .min(10, 'A data de chegada tem que ter 10 caracteres')
+    .max(10, 'A data de chegada tem que ter 10 caracteres')
+    .matches(
+      /^\d{2}\/\d{2}\/\d{4}/,
+      'A data de chegada precisa estar no formato correto (00/00/0000)',
+    ),
+  leaflet: yup
+    .string()
+    .required('A bula é obrigatória')
+    .min(10, 'A bula precisa ter no mínimo 10 caracteres'),
+  provider: yup.string().required('O fornecedor é obrigatório'),
+})
 
 export type MedicineSchema = yup.InferType<typeof medicineSchema>
 
